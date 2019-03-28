a1043c5e13dab6582393dc51fe363d736fa27f89
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-28 17:18:06 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,6 +7,14 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-      return "Pick me!" * 10
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
 end
